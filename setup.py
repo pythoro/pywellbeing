def3ff1ec17a0e886f942049903ecac43ea083dc
pythoro <pythoro@mindquip.com>
2022-08-28 22:30:33 +1200

    [IMP] Package v0.0.3.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="pywellbeing",
-    version="0.0.2",
+    version="0.0.3",
     author="Reuben Rusk",
     author_email="pythoro@mindquip.com",
     description="Computational models of human happiness, wellbeing, and motivation.",
